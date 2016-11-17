445b84db9626668bc2c13c1a7dd71d837e2a815b
Will Morgan <wmorgan@Wills-MacBook.local>
2016-11-17 12:41:38 -0800

    Release 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject epic "0.1.0-SNAPSHOT"
+(defproject epic "0.1.0"
   :dependencies [[org.clojure/clojure "1.8.0"]                 
                  [compojure "1.5.1"]
                  [twilio-api "1.0.1"]
